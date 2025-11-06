4bc67621c10139a967f2bf625315ee503f622923
2023692562-droid <2023692562@student.uitm.edu.my>
2025-11-06 16:38:58 +0800

    second commit

--- technician/config.php
+++ technician/config.php
@@ -1,14 +1,11 @@
 <?php
-// config.php
 $servername = "localhost";
-$username = "root";
-$password = ""; // KOSONGKAN JIKA GUNA XAMPP DEFAULT
-$dbname = "inventory"; // PASTIKAN NAMA DATABASE BETUL
+$username = "nexcheck_dbuser";
+$password = "nexcheck_dbuser";
+$database = "inventory"; // nama database kamu
 
-// Buat sambungan
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $database);
 
-// Semak sambungan
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
